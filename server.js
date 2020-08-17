0ffcfe74176719f0435951a4bde0ce8391f71f13
naama <0naama0@gmail.com>
2020-08-17 14:36:57 -0400

    cors

--- server.js
+++ server.js
@@ -9,11 +9,19 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 const versesController = require("./controllers/versesController");
 const bookController = require('./controllers/bookController')
-app.use(cors());
+//cors
+const corsOptions = {
+  origin: 'https://moodverse.netlify.app'
+}
+app.use(cors(corsOptions));
+app.options('*', cors())
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(logger("dev"));
 app.use(bodyParser.json());
+
+
 
 // Routes
 app.use("/api/users", userController);
