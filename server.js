f3fbd7388c67fdd7389aab91f2e215e20155302e
naama <0naama0@gmail.com>
2020-08-17 15:11:26 -0400

    removed redundancies

--- server.js
+++ server.js
@@ -4,15 +4,12 @@ const app = express();
 const cors = require("cors");
 const logger = require("morgan");
 const userController = require("./controllers/userController");
-// const passport = require("passport")
+
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 const versesController = require("./controllers/versesController");
 const bookController = require('./controllers/bookController')
 //cors
-// const corsOptions = {
-//   origin: 'https://moodverse.netlify.app'
-// }
 app.use(cors());
 app.options('*', cors())
 
@@ -27,7 +24,6 @@ app.use(bodyParser.json());
 app.use("/api/users", userController);
 app.use("/verses", versesController);
 app.use("/api/books", bookController)
-// app.set("port", process.env.PORT || 3000);
 app.listen(PORT, () => {
   console.log(`✅ PORT: ${PORT} 🌟`);
 });
